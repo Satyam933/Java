78e4b6f2a661153113c5a23e6f5af870bd0ab3b3
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-17 05:03:35 +0530

    added escapeSequence.java

---
+++ Basic Codes/escapeSequence.java
@@ -0,0 +1,9 @@
+public class escapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I\'M Satyam Kumar.");
+        System.out.println("Software\tDevelopment   \bEngineer.");
+        System.out.print("\"Chanfigarh University\"");
+        System.out.print("\\Mohali");
+    }
+}
