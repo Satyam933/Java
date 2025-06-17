97c429ec65e450beed8b0c15b15317be5382ebfe
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-17 07:17:09 +0530

    added userInput.java

---
+++ Basic Codes/userInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class userInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nEnter a Integer : ");
+        int i = input.nextInt();
+        System.out.print("Enter a Floating value : ");
+        float f = input.nextFloat();
+        System.out.print("Enter a Character  : ");
+        char c = input.next().charAt(0);
+        input.nextLine();
+        System.out.print("Enter a String : ");
+        String name = input.nextLine();
+
+        System.out.println("\n\nInteger : " + i);
+        System.out.println("Float : " + f);
+        System.out.println("Character : " + c);
+        System.out.println("String : " + name);
+        
+        input.close();
+    }
+}
