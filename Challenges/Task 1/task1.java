ba37ecafb5c508dd5a78b14c87a5d7d8622c881a
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-17 06:20:23 +0530

    hgh

--- Challenges/Task 1/task1.java
+++
@@ -1,7 +0,0 @@
-public class task1 {
-    public static void main(String[] args) {
-        System.out.println("Right Half Pyramid\n*\n**\n***\n****\n*****");
-        System.out.println("Reverse Right Half Pyramid\n*****\n****\n***\n**\n*");
-        System.out.println("Left Half Pyramid\n\t\b\b\b\b*\n   **\n  ***\n ****\n*****");
-    }
-}
