e3b9f92acc187cf0cb78123318a8c3ed0fd07bb9
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-17 06:18:18 +0530

    added description of problem in task1.java

---
+++ Challenges/task1.java
@@ -0,0 +1,18 @@
+//Description : 
+/*
+  Task: 1. 
+
+Show the following patterns using single print statement:
+
+--Right Half Pyramid
+--Reverse Right Half Pyramid
+--Left Half Pyramid
+
+ */
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Right Half Pyramid\n*\n**\n***\n****\n*****");
+        System.out.println("Reverse Right Half Pyramid\n*****\n****\n***\n**\n*");
+        System.out.println("Left Half Pyramid\n\t\b\b\b\b*\n   **\n  ***\n ****\n*****");
+    }
+}
