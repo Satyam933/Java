4fa23f0c9d1d0ed40ad4070cbbf67f87491e065e
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-18 06:43:46 +0530

    added Operators folder

---
+++ Operators/Assignment.java
@@ -0,0 +1,8 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int num = 10; // value 10 is assigned to num;
+        int num1 = num; //value of num is assigned to num1;
+        System.out.println("num : " + num);
+        System.out.println("num1 : "+num1);
+    }
+}
