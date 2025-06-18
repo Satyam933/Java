a472189f275124b1da4770bfcf58bd4b04d1944b
Satyam Kumar <satyamkumar811450@gmail.com>
2025-06-18 06:40:29 +0530

    task3.java added

---
+++ Challenges/task3.java
@@ -0,0 +1,12 @@
+//WAP to swap two numbers.
+public class task3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("VALUE BEFORE SWAPPING : \na = "+a+" b = "+b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("VALUE AFTER SWAPPING : \na = "+a+" b = "+b);
+    }
+}
