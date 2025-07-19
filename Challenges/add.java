5f703a2bb8fc8f53a3a41fb83b752ec66b19d4f3
Satyam Kumar <satyamkumar811450@gmail.com>
2025-07-19 18:47:01 +0530

    updated some files

--- Challenges/add.java
+++ Challenges/add.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class add {
     public static void main(String[] args) {
         System.out.print("Enter two numbers : ");
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
-        System.out.println("Sum : " + (a + b));
-        
-        input.close();
+        try (Scanner input = new Scanner(System.in)) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            System.out.println("Sum : " + (a + b));
+            
+            input.close();
+        }
         }
 }
--- Operators/ShorthandOperator.java
+++ Operators/ShorthandOperator.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class ShorthandOperator {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int num1 = input.nextInt();
-        System.out.print("Value of num1 : " + num1+"\n");
-        System.out.print("Enter a number which you want to add in num1 : ");
-        int num2 = input.nextInt();
-        num1 += num2; //Shorthand Operator.
-        System.out.print("Sum : "+num1);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a number : ");
+            int num1 = input.nextInt();
+            System.out.print("Value of num1 : " + num1+"\n");
+            System.out.print("Enter a number which you want to add in num1 : ");
+            int num2 = input.nextInt();
+            num1 += num2; //Shorthand Operator.
+            System.out.print("Sum : "+num1);
+        }
     }
 }
