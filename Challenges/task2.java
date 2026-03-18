5eb70b926f8d08c6c9f17431a5ad19b2e23a13e7
Satyam Kumar <satyamkumar811450@gmail.com>
2026-03-18 13:05:44 +0530

    changes add.java---->task2.java

--- Challenges/task2.java
+++ Challenges/task2.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class add {
+public class task2 {
     public static void main(String[] args) {
         System.out.print("Enter two numbers : ");
         try (Scanner input = new Scanner(System.in)) {
