78e751bf6bbcab8f8396ecb2b7d5b8640131c694
Satyam Kumar <satyamkumar811450@gmail.com>
2026-03-31 14:08:01 +0530

    added how jagged array works

---
+++ OOPs/OOP5JagedArray.java
@@ -0,0 +1,23 @@
+package OOPs;
+
+public class OOP5JagedArray {
+  public static void main(String[] args) {
+    int arr[][] = new int[3][]; //jagged array
+    arr[0] = new int[3];
+    arr[1] = new int[4];
+    arr[2] = new int[3];
+    
+    for (int n[] : arr) {
+      for (int j = 0; j < n.length; j++) {
+        n[j] = (int) (Math.random() * 10);
+      }
+    }
+    
+    for (int n[] : arr) {
+      for (int m : n) {
+        System.out.print(m + " ");
+      }
+      System.out.println();
+    }
+  }
+}
